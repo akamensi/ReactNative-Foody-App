01ac3b4e14015ab9982c28f0521a5e0c6ee40340
akamensi <akez.muhammed@gmail.com>
2025-06-13 16:48:43 +0100

    Showinf a list of images

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,14 +1,14 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList, Image } from "react-native";
 import { useEffect, useState } from "react";
 import yelp from "../api/yelp";
 
 const ResultsShowScreen = ({ route }) => {
   const { id } = route.params || {}; // Safely destructure `id` (fallback to empty object)
-  const [results, setResults] = useState(null);
+  const [result, setResult] = useState(null);
 
   const getResultById = async (id) => {
     const response = await yelp.get(`/${id}`);
-    setResults(response.data);
+    setResult(response.data);
   };
 
   useEffect(() => {
@@ -17,14 +17,29 @@ const ResultsShowScreen = ({ route }) => {
     }
   }, []);
 
-  console.log("ResultsShowScreen", results);
+  if (!result) {
+    return null;
+  }
+
   return (
     <View>
-      <Text>ResultsShowScreen</Text>
+      <Text>{result.name}</Text>
+      <FlatList
+        data={result.photos}
+        keyExtractor={(photo) => photo}
+        renderItem={({ item }) => {
+          return <Image style={styles.imageStyle} source={{ uri: item }} />;
+        }}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imageStyle: {
+    width: 300,
+    height: 200
+  }
+});
 
 export default ResultsShowScreen;
